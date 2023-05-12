7effc75f00b3ba8b8d3ff1f6afe3353fbea3c8ba
Rafael <80691464+Rafael-Franchini@users.noreply.github.com>
2023-05-12 00:17:39 -0300

    criação do arquivo base

--- trabalhoPOO/trabalhoPOO.cpp
+++ trabalhoPOO/trabalhoPOO.cpp
@@ -1,9 +1,32 @@
 
 #include <iostream>
-#include<iomanip>
+
 using namespace std;
-int main()
-{
-	cout << "FUNFO";
+
+int main(){
+
+	int op;
+	do{
+		cin>>op;
+		switch (op){
+			case 1:
+				cout<<"Voce escolheu a opção 1"<<endl; 
+				break;
+			
+			case 2:
+				cout<<"Voce escolheu a opção 2"<<endl; 
+				break;
+
+			case 3:
+				cout<<"pulando fora"<<endl;
+				break;
+
+			default:
+				cout<<"Opção invalida tente novamente"<<endl;
+				break;
+		}
+	}while(op !=3);
+	return 0;
 }
+
 
